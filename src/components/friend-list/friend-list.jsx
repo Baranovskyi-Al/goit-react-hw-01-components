ffd797b9e5bc5d9a70045e3f987bd8c132aac2e7
Oleksandr Baranovskyi <89201309+Baranovskyi-Al@users.noreply.github.com>
2022-05-05 22:16:41 +0300

    refactoring friend-list

--- src/components/friend-list/friend-list.jsx
+++ src/components/friend-list/friend-list.jsx
@@ -1,38 +1,33 @@
 import styles from './friend-list.module.css';
 import PropTypes from 'prop-types';
+import FriendListItem from './friendListItem';
 
-const FriendList = ({ friends }) => (
-  <section>
-    <ul className={styles.list}>
-      {friends.map(friend => (
-        <li className={styles.item} key={friend.id}>
-          <span
-            className={
-              friend.isOnline ? styles.statusOnline : styles.statusOffline
-            }
-          ></span>
-          <img
-            className={styles.avatar}
-            src={friend.avatar}
-            alt="Avatar"
-            width="48"
+const FriendList = ({ friends }) => {
+  return (
+    <section>
+      <ul className={styles.list}>
+        {friends.map(({ avatar, name, isOnline, id }) => (
+          <FriendListItem
+            avatar={avatar}
+            name={name}
+            isOnline={isOnline}
+            key={id}
           />
-          <p className={styles.name}>{friend.name}</p>
-        </li>
-      ))}
-    </ul>
-  </section>
-);
+        ))}
+      </ul>
+    </section>
+  );
+};
 
 FriendList.propTypes = {
   friends: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.number.isRequired,
+    PropTypes.exact({
       avatar: PropTypes.string.isRequired,
       name: PropTypes.string.isRequired,
       isOnline: PropTypes.bool.isRequired,
+      id: PropTypes.number.isRequired,
     })
-  ),
+  ).isRequired,
 };
 
 export default FriendList;
