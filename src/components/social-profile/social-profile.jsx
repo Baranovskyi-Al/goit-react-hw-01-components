e896791b32fc122cc5d5bfa208de23c4278cecc8
Oleksandr Baranovskyi <89201309+Baranovskyi-Al@users.noreply.github.com>
2022-05-05 21:28:17 +0300

    add destructuring stats

--- src/components/social-profile/social-profile.jsx
+++ src/components/social-profile/social-profile.jsx
@@ -2,6 +2,7 @@ import styles from './social-profile.module.css';
 import PropTypes from 'prop-types';
 
 const SocialProfile = ({ avatar, username, tag, location, stats }) => {
+  const { followers, views, likes } = stats;
   return (
     <div className={styles.profile}>
       <div className={styles.description}>
@@ -14,15 +15,15 @@ const SocialProfile = ({ avatar, username, tag, location, stats }) => {
       <ul className={styles.stats}>
         <li className={styles.item}>
           <span className={styles.label}>Followers</span>
-          <span className="quantity">{stats.followers}</span>
+          <span className="quantity">{followers}</span>
         </li>
         <li className={styles.item}>
           <span className={styles.label}>Views</span>
-          <span className="quantity">{stats.views}</span>
+          <span className="quantity">{views}</span>
         </li>
         <li className={styles.item}>
           <span className={styles.label}>Likes</span>
-          <span className="quantity">{stats.likes}</span>
+          <span className="quantity">{likes}</span>
         </li>
       </ul>
     </div>
